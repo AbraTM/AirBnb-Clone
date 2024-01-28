4540f094e607215c32cd54dad51ac2302f4452e5
AbraTM <tusharmalhan2564@gmail.com>
2024-01-28 17:03:19 +0530

    fix: images

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero(){
     return(
         <div className="hero">
-            <img src="../images/photo-grid.png" alt=""></img>
+            <img src="./images/photo-grid.png" alt=""></img>
             <div className="text">
                 <h1>Online Experiences</h1>
                 <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
